245455afd3a2131344aa9f5b3cd5e3060efbdea6
aleksbn <aleksbn@users.noreply.github.com>
2023-11-24 15:54:24 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './components/pages/home';
+import NavBar from './components/layout/navbar';
+import NotFound from './components/pages/notFound';
+import LoginForm from './components/pages/login';
+import Games from './components/pages/games';
+import History from './components/pages/history';
+import Register from './components/pages/register';
+
+import auth from './services/authService';
+
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css';
+import ProtectedRoute from './components/common/protectedRoute';
+import Logout from './components/pages/logout';
+
+class App extends Component {
+  state = {
+    user: null
+  };
+
+  componentDidMount() {
+    const user = auth.getCurrentUser();
+    this.setState({ user });
+  }
+
+  render() {
+    const { user } = this.state;
+    return (
+      <React.Fragment>
+        <ToastContainer />
+        <NavBar user={user} />
+        <main className="container">
+          <Switch>
+            <Route path="/home" component={Home} />
+            <ProtectedRoute path="/games" component={Games}/>
+            <ProtectedRoute path="/history" component={History}/>
+            <Route path="/register" component={Register} />
+            <Route path="/not-found" component={NotFound} />
+            <Route path="/login" component={LoginForm} />
+            <Route path="/logout" component={Logout} />
+            <Redirect from="/" exact to="/home" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
